c21b804fec078f3965320004d911258789702acd
sofiamfernandez <68400669+sofiamfernandez@users.noreply.github.com>
2020-07-21 17:19:57 +0200

    Add files via upload

---
+++ build/precache-manifest.0bf763f2c56ad192df25991089393173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23175d40054c11b70dea46b8b17ea184",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45d335dc83e3a85959c8",
+    "url": "/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "0012f5a03a153668762a",
+    "url": "/static/js/2.2aca169f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2aca169f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d335dc83e3a85959c8",
+    "url": "/static/js/main.356ca11f.chunk.js"
+  },
+  {
+    "revision": "9078284b3c5a7cc4441c",
+    "url": "/static/js/runtime-main.3c7fdb96.js"
+  }
+]);
